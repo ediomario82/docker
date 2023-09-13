cc9294f80b83c94be3fb58692d4dbce57f14fa79
ediomario82 <144544973+ediomario82@users.noreply.github.com>
2023-09-13 10:39:03 -0300

    Update uptimekuma.sh

--- uptimekuma.sh
+++ uptimekuma.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-docker create volume uptime-kuma
+cd
+
+docker volume create uptime-kuma
 
 docker run -d \
              -m 512m \
              --memory-swap 1G \
              --cpus 0.25 \
               --restart=always \
-              -p 3001:3001
+              -p 3001:3001 \
               -v uptime-kuma:/app/data \
-              --name uptime-kuma louislam/uptime-kuma:1
+              --name kuma louislam/uptime-kuma:latest
