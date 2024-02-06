5ff0800fc5c7fad6a7ab5fd276f5b161a8c048c4
ediomario82 <144544973+ediomario82@users.noreply.github.com>
2024-02-06 17:58:32 -0300

    Create xteve.sh

---
+++ xteve.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker run -d \
+	--name=xteve_lazystream \
+	-p 34400:34400 \
+	-e TZ="America/Sao_Paulo" \
+	--env-file=.env \
+	--log-opt max-size=10m \
+	--log-opt max-file=3 \
+	-v /mnt/user/appdata/xteve/.xteve:/xteve:rw \
+	-v /mnt/user/appdata/xteve/config/:/config:rw \
+	-v /mnt/user/appdata/xteve/guide2go/:/guide2go:rw \
+	-v /mnt/user/appdata/xteve/playlists/:/playlists:rw \
+	-v /tmp/xteve/:/tmp/xteve:rw \
+	taylorbourne/xteve_lazystream
+ 
+# https://github.com/taylorbourne/xteve_lazystream/tree/master
