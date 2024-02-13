2aa6179f7d05fbf45096cabb01843b137f0b30f7
ediomario82 <144544973+ediomario82@users.noreply.github.com>
2024-02-12 21:23:02 -0300

    Update zab6+mysql.sh

--- zab6+mysql.sh
+++ zab6+mysql.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 # Criar rede zabbix-net
-docker network create zabbix-net --subnet 172.20.0.0/16 --ip-range 172.20.240.0/20 
-
-docker volume create dbData
+docker network create zabbix-net --subnet 172.20.0.0/16 --ip-range 172.20.240.0/20
 
 docker run --name mysql-server -t \
              -e MYSQL_DATABASE="zabbix" \
              -e MYSQL_USER="zabbix" \
              -e MYSQL_PASSWORD="zabbix_pwd" \
              -e MYSQL_ROOT_PASSWORD="root_pwd" \
+             --network=zabbix-net \
              -v dbData:/var/lib/mysql \
+             --restart unless-stopped \
+             -d mysql:8.0.36 \
+             --character-set-server=utf8mb4 --collation-server=utf8mb4_bin \
+
+docker run --name zabbix-server-mysql -t \
+             -e DB_SERVER_HOST="mysql-server" \
+             -e MYSQL_DATABASE="zabbix" \
+             -e MYSQL_USER="zabbix" \
+             -e MYSQL_PASSWORD="zabbix_pwd" \
+             -e MYSQL_ROOT_PASSWORD="root_pwd" \
              --network=zabbix-net \
              --restart unless-stopped \
-             -d mysql:8.0 \
-             --character-set-server=utf8 --collation-server=utf8_bin \
-             --default-authentication-plugin=mysql_native_password
-
+             -d zabbix/zabbix-server-mysql:alpine-6.0-latest
 docker run --name zabbix-java-gateway -t \
              --network=zabbix-net \
              --restart unless-stopped \
@@ -34,18 +40,6 @@ docker run --name zabbix-server-mysql -t \
              --restart unless-stopped \
              -d zabbix/zabbix-server-mysql:alpine-6.0-latest
 
-docker run --name zabbix-web-nginx-mysql -t \
-             -e ZBX_SERVER_HOST="zabbix-server-mysql" \
-             -e DB_SERVER_HOST="mysql-server" \
-             -e MYSQL_DATABASE="zabbix" \
-             -e MYSQL_USER="zabbix" \
-             -e MYSQL_PASSWORD="zabbix_pwd" \
-             -e MYSQL_ROOT_PASSWORD="root_pwd" \
-             --network=zabbix-net \
-             -p 80:8080 \
-             --restart unless-stopped \
-             -d zabbix/zabbix-web-nginx-mysql:alpine-6.0-latest
-
 # Criar container Grafana
 docker volume create grafana-storage
 
@@ -55,7 +49,8 @@ docker run --name grafana \
            -e "GF_INSTALL_PLUGINS=alexanderzobnin-zabbix-app" \
            -e "GF_PLUGINS_ALLOW_LOADING_USIGNED_PLUGINS=alexanderzobnin-zabbix-datasource" \
            --network=zabbix-net \
-	         --restart unless-stopped \
+           --restart unless-stopped \
            -p 3000:3000 \
            -v grafana-storage:/var/lib/grafana \
            -d grafana/grafana-enterprise
+      
