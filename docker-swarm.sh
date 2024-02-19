b6a848f08457f43e48d3e1a88c9a2c9ba7b25965
Ediomario <ediomario.oliveira@linktelwifi.com>
2024-02-19 10:40:24 -0300

    adcionado configurações swarm.

--- docker-swarm.sh
+++ docker-swarm.sh
@@ -60,3 +60,18 @@ volumes:
   postgres_data:
 
 
+# Criar volume para manter dados presistente no container
+cd
+docker volume create uptime-kuma
+# Criar Network
+docker network create -d overlay swarm-net
+
+# Criar container com configurações restritas
+docker service create \
+        --replicas 4 \
+        --network swarm-net \
+        --constraint  'node.role == worker' \
+        --mount type=volume,source=uptime-kuma,destination=/app/data \
+        -p 8000:3001 \
+        --name kuma \
+        louislam/uptime-kuma:latest
