3dbe91f7eaaf683fe356a69031461048ce52fe7b
ediomario82 <144544973+ediomario82@users.noreply.github.com>
2026-05-18 22:55:28 -0300

    Create open-webui.sh

---
+++ open-webui.sh
@@ -0,0 +1,9 @@
+docker run -d \
+        --network=host \
+        --gpus all \
+        -v open-webui:/app/backend/data \
+        -e OLLAMA_BASE_URL=http://127.0.0.1:11434 \ 
+        --name open-webui \
+        --restart always \
+        ghcr.io/open-webui/open-webui:cuda
+
